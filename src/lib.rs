33871d4225f4d2f4006c3b8f1bbb86b92f98cbd6
Jan Max Meyer <jmm@phorward.de>
2022-06-28 17:47:19 +0200

    Added radix to internal- and radix-enabled functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
+//! JavaScript-style parseInt/parseFloat prefix parsing of strings for Rust
+
 use num;
 
-/// JavaScript parseInt-style prefix parsing
-
-fn parse_uint_from_iter_internal<T: num::PrimInt>(chars: &mut dyn Iterator<Item = char>) -> T {
+/// Internal function to parse uint values from a char-iterator with a given radix.
+fn parse_uint_internal<T: num::PrimInt>(chars: &mut dyn Iterator<Item = char>, radix: u32) -> T {
     let mut ret = T::zero();
 
     while let Some(ch) = chars.next() {
-        match ch.to_digit(10) {
+        match ch.to_digit(radix) {
             Some(digit) => {
-                ret = ret.checked_mul(&T::from(10).unwrap()).unwrap();
+                ret = ret.checked_mul(&T::from(radix).unwrap()).unwrap();
                 ret = ret.checked_add(&T::from(digit).unwrap()).unwrap();
             }
             None => break,
@@ -18,7 +19,11 @@ fn parse_uint_from_iter_internal<T: num::PrimInt>(chars: &mut dyn Iterator<Item 
     ret
 }
 
-pub fn parse_uint_from_iter<T: num::PrimInt>(chars: &mut dyn Iterator<Item = char>) -> T {
+/// Parse uint values from an iterator with a given radix.
+pub fn parse_uint_from_iter_with_radix<T: num::PrimInt>(
+    chars: &mut dyn Iterator<Item = char>,
+    radix: u32,
+) -> T {
     let mut chars = chars.peekable();
 
     while let Some(ch) = chars.peek() {
@@ -27,14 +32,21 @@ pub fn parse_uint_from_iter<T: num::PrimInt>(chars: &mut dyn Iterator<Item = cha
             continue;
         }
 
-        break
+        break;
     }
 
-    parse_uint_from_iter_internal::<T>(&mut chars)
+    parse_uint_internal::<T>(&mut chars, radix)
 }
 
-pub fn parse_int_from_iter<T: num::PrimInt + std::ops::Neg<Output = T>>(
+/// Parse decimal uint values from an iterator.
+pub fn parse_uint_from_iter<T: num::PrimInt>(chars: &mut dyn Iterator<Item = char>) -> T {
+    parse_uint_from_iter_with_radix(chars, 10)
+}
+
+/// Parse int values from an iterator with a given radix.
+pub fn parse_int_from_iter_with_radix<T: num::PrimInt + std::ops::Neg<Output = T>>(
     chars: &mut dyn Iterator<Item = char>,
+    radix: u32,
 ) -> T {
     let mut chars = chars.peekable();
     let mut neg = false;
@@ -53,7 +65,7 @@ pub fn parse_int_from_iter<T: num::PrimInt + std::ops::Neg<Output = T>>(
         break;
     }
 
-    let ret = parse_uint_from_iter_internal::<T>(&mut chars);
+    let ret = parse_uint_internal::<T>(&mut chars, radix);
 
     if neg {
         -ret
@@ -62,12 +74,31 @@ pub fn parse_int_from_iter<T: num::PrimInt + std::ops::Neg<Output = T>>(
     }
 }
 
-pub fn parse_uint<T: num::PrimInt>(s: &str) -> T {
-    parse_uint_from_iter(&mut s.chars())
+/// Parse decimal int values from an iterator.
+pub fn parse_int_from_iter<T: num::PrimInt + std::ops::Neg<Output = T>>(
+    chars: &mut dyn Iterator<Item = char>,
+) -> T {
+    parse_int_from_iter_with_radix::<T>(chars, 10)
 }
 
+/// Parse uint values from a &str with a given radix.
+pub fn parse_uint_with_radix<T: num::PrimInt>(s: &str, radix: u32) -> T {
+    parse_uint_from_iter_with_radix::<T>(&mut s.chars(), radix)
+}
+
+/// Parse decimal uint values from a &str.
+pub fn parse_uint<T: num::PrimInt>(s: &str) -> T {
+    parse_uint_from_iter_with_radix::<T>(&mut s.chars(), 10)
+}
+
+/// Parse int values from a &str with a given radix.
+pub fn parse_int_with_radix<T: num::PrimInt + std::ops::Neg<Output = T>>(s: &str, radix: u32) -> T {
+    parse_int_from_iter_with_radix::<T>(&mut s.chars(), radix)
+}
+
+/// Parse decimal int values from a &str.
 pub fn parse_int<T: num::PrimInt + std::ops::Neg<Output = T>>(s: &str) -> T {
-    parse_int_from_iter(&mut s.chars())
+    parse_int_from_iter_with_radix::<T>(&mut s.chars(), 10)
 }
 
 #[test]
