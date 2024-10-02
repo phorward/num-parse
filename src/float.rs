fd7a9d567bbb114063de872ebefdbe11938b8d6f
Jan Max Meyer <jmm@phorward.de>
2024-10-02 02:08:48 +0200

    cargo fmt

--- src/float.rs
+++ src/float.rs
@@ -125,8 +125,7 @@ pub fn parse_float_from_iter<T: num::Float + num::FromPrimitive + std::fmt::Disp
             for _ in 0..exp {
                 if neg {
                     ret = ret / ten;
-                }
-                else {
+                } else {
                     ret = ret * ten;
                 }
             }
@@ -182,12 +181,18 @@ fn test_parse_float_f64() {
         Some(-0.000000000000001337f64)
     );
     assert_eq!(parse_float::<f64>(" -1337.0e-30f64 "), Some(-1337.0e-30f64));
-    assert_eq!(parse_float::<f64>(" -1337.0e-296f64 "), Some(-1337.0e-296f64));  // OK
-    assert_eq!(parse_float::<f64>(" -1337.0e-297f64 "), Some(-1337.0e-297f64));  // FAIL
-    /*
     assert_eq!(
-        parse_float::<f32>(" -1337.0e-326f32 "),
-        Some(-1337.0e-326f32)
-    );
-    */
+        parse_float::<f64>(" -1337.0e-296f64 "),
+        Some(-1337.0e-296f64)
+    ); // OK
+    assert_eq!(
+        parse_float::<f64>(" -1337.0e-297f64 "),
+        Some(-1337.0e-297f64)
+    ); // FAIL
+       /*
+       assert_eq!(
+           parse_float::<f32>(" -1337.0e-326f32 "),
+           Some(-1337.0e-326f32)
+       );
+       */
 }
